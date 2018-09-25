b717b37c643e323ff38dc1cbd1e87a68a4959c9d
umeshkumar99 <36888531+umeshkumar99@users.noreply.github.com>
2018-09-25 17:50:49 +0530

    opneclosedprincipal

---
+++ EmailDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace EmailDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.bizmail.yahoo.com");
+
+                mail.From = new MailAddress("marketing@progenbiologics.com");
+                mail.To.Add("umesh.kumar99@gmail.com");
+                mail.Subject = "Test message Report";
+                mail.Body = "Report";
+               // Attachment attachment = new Attachment(filename);
+               // mail.Attachments.Add(attachment);
+
+                SmtpServer.Port =587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("marketing@progenbiologics.com", "Hari@082017");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+            }
+            catch(Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ OpenClosedPrincipal/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedPrincipal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class MileageCalculator
+    {
+        IEnumerable<Car> _cars;
+        public MileageCalculator(IEnumerable<Car> cars) { this._cars = cars; }
+
+        public void CalculateMileage()
+        {
+            CarController controller = new CarController();
+            foreach (var car in _cars)
+            {
+                Console.WriteLine("Mileage of the car {0} is {1}", car.Name, controller.GetCarMileage(car.Name));
+            }
+        }
+    }
+
+    public class CarController
+    {
+        List<ICar> cars;
+        public CarController()
+        {
+            cars = new List<ICar>();
+            cars.Add(new Audi());
+            cars.Add(new Mercedes());
+        }
+
+        public string GetCarMileage(string name)
+        {
+            return cars.First(car => car.Name == name).GetMileage();
+        }
+    }
+
+    public interface ICar
+    {
+        string Name { get; set; }
+        string GetMileage();
+    }
+
+    public class Audi : ICar
+    {
+        public string Name { get; set; }
+
+        public string GetMileage()
+        {
+            return "10M";
+        }
+    }
+
+    public class Mercedes : ICar
+    {
+        public string Name { get; set; }
+
+        public string GetMileage()
+        {
+            return "20M";
+        }
+    }
+}
